29430880384751557ef779b8a01072c57a9d1fe0
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-09 08:29:55 +0200

    Form step 2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Form = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
