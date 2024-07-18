3d60972b12feb136c9ba462d5f656c3ded82ba2f
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-07-18 17:35:44 +0200

    alert when click on a button and close afterwards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert
+          onClose={() => setAlertVisibility(false)}
+          children="This is an alert"
+        />
+      )}
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>
         My Button to click on
       </Button>
+      <Alert>This is also an alert but without the onClose functionality</Alert>
     </>
   );
 }
